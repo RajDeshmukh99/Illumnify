2760bbb9a73fe2f92fa558c681ca18f8a93e9807
Rajvardhan99 <rajvardhan1999@gmail.com>
2019-05-05 18:19:39 +0530

    aos anim

--- js/index.js
+++ js/index.js
@@ -2,3 +2,35 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+ /* $(window).on("load",function()
+     {
+       $(window).scroll(function()
+       {
+         var windowBottom= $(this).scrollTop() + $(this).innerHeight();
+         $(".new").each(function() {
+           var objectBottom= $(this).offset.top + $(this).outerHeight();
+
+           if(objectBottom < windowBottom)
+           {
+             //if($(this).css("opacity")==0)
+             $(this).fadeTo(250,1);
+           }
+           else{
+           //{if($(this).css("opacity")==1)
+           //{
+             $(this).fadeTo(250,0)
+           //}
+           }
+         });
+       }).scroll();
+     });*/
+
+    
+     $(window).scroll(function() {
+      if ($(this).scrollTop() > 250 && $(this).scrollTop() < 1250) {
+        $(".new").fade(500);
+      } else {
+        $(".new").fadeOut(500);
+      }
+    });
