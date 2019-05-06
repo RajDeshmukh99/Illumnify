3579a70fa622087c8c32cce4be1cb9477ec8df5e
Rishav1999 <rishav.agarwal2017@vitstudent.ac.in>
2019-05-07 01:58:25 +0530

    animate_logo_change

--- js/index.js
+++ js/index.js
@@ -15,6 +15,13 @@ window.addEventListener('scroll', function(e) {
   }
 });
 
+$(document).ready(function(){
+  $( "a.scrollLink" ).click(function( event ) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+  });
+});
+
  /* $(window).on("load",function()
      {
        $(window).scroll(function()
