919c596fbe253bb27f0ab2f72db41efbd122114e
Rajvardhan99 <rajvardhan1999@gmail.com>
2019-05-16 20:30:31 +0530

    touch ups

--- js/index.js
+++ js/index.js
@@ -1,56 +1,33 @@
-$('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('#overlay').toggleClass('open');
-  });
-  
-$( '.menu li a' ).on("click", function(){
-  $('#toggle').click();
+// ----------------------------------NAV BAR--------------------------------
+$("#toggle").click(function() {
+  $(this).toggleClass("active");
+  $("#overlay").toggleClass("open");
 });
 
-window.addEventListener('scroll', function(e) {
-  if( $(window).scrollTop() <= 50) {
-      $('.wow').removeClass('animated');
-      $('.wow').removeAttr('style');
-      new WOW().init();
+$(".menu li a").on("click", function() {
+  $("#toggle").click();
+});
+
+// --------------------------------------WOW JS-----------------------------------------------------
+
+window.addEventListener("scroll", function(e) {
+  if ($(window).scrollTop() <= 50) {
+    $(".wow").removeClass("animated");
+    $(".wow").removeAttr("style");
+    new WOW().init();
   }
 });
 
-$(document).ready(function(){
-  $( "a.scrollLink" ).click(function( event ) {
-      event.preventDefault();
-      $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+// -----------------------------PARALLAX AND NAV SCROLL------------------------------------
+
+$(document).ready(function() {
+  $(".parallax").parallax();
+  console.log(" jjsdbjsjdbfkjb");
+  $("a.scrollLink").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500
+    );
   });
 });
-
-
- /* $(window).on("load",function()
-     {
-       $(window).scroll(function()
-       {
-         var windowBottom= $(this).scrollTop() + $(this).innerHeight();
-         $(".new").each(function() {
-           var objectBottom= $(this).offset.top + $(this).outerHeight();
-
-           if(objectBottom < windowBottom)
-           {
-             //if($(this).css("opacity")==0)
-             $(this).fadeTo(250,1);
-           }
-           else{
-           //{if($(this).css("opacity")==1)
-           //{
-             $(this).fadeTo(250,0)
-           //}
-           }
-         });
-       }).scroll();
-     });*/
-
-    
-     /*$(window).scroll(function() {
-      if ($(this).scrollTop() > 250 && $(this).scrollTop() < 1250) {
-        $(".new").fade(500);
-      } else {
-        $(".new").fadeOut(500);
-      }
-    });*/
